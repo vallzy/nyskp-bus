7ea6e6ea49268d5f7e14b99b3b2507f8f52e3ec5
vallzy <45981588+vallzy@users.noreply.github.com>
2022-08-03 15:14:22 +0000

    Frontend tree view
    Frontend tree view which shows currently accessed files.

--- src/components/GetAccess.js
+++ src/components/GetAccess.js
@@ -1,73 +1,79 @@
-import { Button, Card, Switch, Tree } from 'antd';
-import React, { useState } from 'react';
+import { Button, Card, Switch, Tree, Typography, Divider } from 'antd';
+import React, { useState, useEffect } from 'react';
 const { DirectoryTree } = Tree;
+const { Title } = Typography;
 
-const treeData = [
-    {
-      title: 'sandbox',
-      key: '0-0',
-      children: [
-        {
-          title: 'output.bus',
-          key: '0-0-0',
-          isLeaf: true,
-        },
-        {
-          title: 'ec.ts',
-          key: '0-0-1',
-          isLeaf: true,
-        },
-      ],
-    },
-    {
-      title: 'tmp',
-      key: '0-1',
-      children: [
-        {
-          title: 'leaf 1-0',
-          key: '0-1-0',
-          isLeaf: true,
-        },
-        {
-          title: 'leaf 1-1',
-          key: '0-1-1',
-          isLeaf: true,
-        },
-      ],
-    },
-  ];
+var treeData = [];
 
 async function handleDirectoryEntry( dirHandle, out ) {
-for await (const entry of dirHandle.values()) {
-    if (entry.kind === "file"){
-    const file = await entry.getFile();
-    out[ file.name ] = file;
-    }
-    if (entry.kind === "directory") {
-    const newOut = out[ entry.name ] = {};
-    await handleDirectoryEntry( entry, newOut );
-    }
+  for await (const entry of dirHandle.values()) {
+      if (entry.kind === "file"){
+      const file = await entry.getFile();
+      out[ file.name ] = file;
+      }
+      if (entry.kind === "directory") {
+      const newOut = out[ entry.name ] = { visited: false, name: entry.name };
+      await handleDirectoryEntry( entry, newOut );
+      }
+  }
 }
+
+function BFS(node) {
+  let counter = 0;
+  let tr = [];
+  let dirQueue = [node];
+  node.name = 'root';
+  while(dirQueue.length > 0) {
+    var nodeIndex = dirQueue.shift();
+    for (const key of Object.keys(nodeIndex)) {
+      const item = nodeIndex[key];
+      if(item.visited !== undefined && !item.visited) { 
+        dirQueue.push(item);
+      }
+    }
+    nodeIndex.visited = true;
+    tr.push({
+      title: nodeIndex.name,
+      key: 'dir-' + nodeIndex.name,
+      children: PopulateRoot(nodeIndex, counter)
+    });
+
+    counter += 1;
+  }
+  treeData = tr;
+  return treeData;
 }
+
 
 async function GetDirectoryAccess() {
     const out = {};
     let dirHandle = await window.showDirectoryPicker();
     await handleDirectoryEntry( dirHandle, out );
-    let tree = GenerateTree(out);
+    let tree = BFS(out);
+    return tree;
 }
 
-async function GenerateTree(rootDir) {
-    const cleanTree = [];
-    for (const key of Object.keys(rootDir)) {
-        const item = rootDir[key];
-        if(item instanceof File) {
-            
-        }
+function PopulateRoot(dir, num) {
+  let content = [];
+  let counter = 0;
+  for (const key of Object.keys(dir)) {
+    const item = dir[key];
+    if(item instanceof File) {
+        content.push({ title: key, key: num + '-' + counter, isLeaf:true });
+        counter += 1;
     }
+  }
+  return content;
 }
 
-const GetAccess = () => (
+
+function GetAccess() {
+  const [data, setData] = useState(treeData);
+  const startAccess = async () => {
+    let y = await GetDirectoryAccess();
+    setData(y);
+  };
+  return(
     <>
     <Card
       title="Bustools Text"
@@ -78,16 +84,21 @@ const GetAccess = () => (
         margin: 5
       }}
     >
-      <Button type="primary" style={{margin: 5}} onClick={GetDirectoryAccess}>Access Directory</Button>
+      <Button type="primary" style={{margin: 5}} onClick={startAccess}>Access Directory</Button>
       <Button type="primary" style={{margin: 5}}>Access File</Button>
-
+      <Divider/>
+      {data.length <= 0 &&
+        <Title level={5} type='danger'>
+          Currently there are no directories with access.
+        </Title>
+      }
       <DirectoryTree
       multiple
       defaultExpandAll
-      treeData={treeData}
+      treeData={data}
     />
     </Card>
     </>
-);
-
+  );
+}
 export default GetAccess;
